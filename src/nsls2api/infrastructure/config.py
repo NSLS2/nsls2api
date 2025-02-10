a42fb84a150acdcfc0b599610eb29729a1dff7fa
Stuart Campbell <scampbell@bnl.gov>
2025-02-10 16:34:08 -0500

    Apply ruff linter

--- src/nsls2api/infrastructure/config.py
+++ src/nsls2api/infrastructure/config.py
@@ -79,8 +79,7 @@ class Settings(BaseSettings):
 
 @lru_cache()
 def get_settings() -> Settings:
-    """
-    Retrieve the settings dictionary.
+    """Retrieve the settings dictionary.
 
     :returns: The dictionary of current settings.
     """
