6e68bd00628fb40036b2601ba0718cd975b95b9a
Aditi Yashwant Chikkali <achikkali1@runansible1.nsls2.bnl.gov>
2025-11-21 09:59:03 -0500

    changed ldap server configs

--- src/nsls2api/api/v1/whoami_api.py
+++ src/nsls2api/api/v1/whoami_api.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Query
 from nsls2api.services.ldap_service import get_user_info
 router = APIRouter()
-
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="src/nsls2api/.env")
 @router.get("/whoami")
-async def whoami(request: Request):
-    #ldap logic
-    LDAP_SERVER = "ldap://ldapproxy.nsls2.bnl.gov"
-    BASE_DN = "dc=bnl,dc=gov"
-    BIND_USER = "CN=n2snbind3,OU=CAM - Service Accounts,OU=CAM,DC=bnl,DC=gov"
-    BIND_PASSWORD = "V3%aKntg8woNa*b3#J"
-    upn = "swilkins@bnl.gov"
+async def whoami(request: Request, upn: str = Query(..., description="User Principal Name")):
+    LDAP_SERVER = os.getenv("LDAP_SERVER")
+    BASE_DN = os.getenv("BASE_DN")
+    BIND_USER = os.getenv("BIND_USER")
+    BIND_PASSWORD = os.getenv("LDAP_BIND_PASSWORD")
+    
 
     user_info = get_user_info(upn, LDAP_SERVER, BASE_DN, BIND_USER, BIND_PASSWORD)
     return {"user": user_info}
