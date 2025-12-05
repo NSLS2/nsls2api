06fd2274791b89ee29401d7732f91c08e6f5f00c
Aditi Yashwant Chikkali <achikkali1@runansible1.nsls2.bnl.gov>
2025-12-05 13:37:57 -0500

    improve ldap connection handling && refactor api to use config settings

--- src/nsls2api/api/v1/whoami_api.py
+++ src/nsls2api/api/v1/whoami_api.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter, Depends, Request, Query
 from nsls2api.services.ldap_service import get_user_info, shape_ldap_response
-router = APIRouter()
-import os
-from dotenv import load_dotenv
+from nsls2api.infrastructure.config import get_settings
 
 router = APIRouter()
-load_dotenv(dotenv_path="src/nsls2api/.env")
 
 @router.get("/whoami")
 async def whoami(request: Request, upn: str = Query(..., description="User Principal Name")):
-    LDAP_SERVER = os.getenv("LDAP_SERVER")
-    BASE_DN = os.getenv("BASE_DN")
-    BIND_USER = os.getenv("BIND_USER")
-    BIND_PASSWORD = os.getenv("LDAP_BIND_PASSWORD")
+    settings = get_settings()
+
+    LDAP_SERVER = settings.ldap_server
+    BASE_DN = settings.base_dn
+    BIND_USER = settings.bind_user
+    BIND_PASSWORD = settings.ldap_bind_password
 
     user_info = get_user_info(upn, LDAP_SERVER, BASE_DN, BIND_USER, BIND_PASSWORD)
     
@@ -21,7 +20,9 @@ async def whoami(request: Request, upn: str = Query(..., description="User Princ
 
     shaped_info = shape_ldap_response(user_info)
     if shaped_info["identity"]["unix"]["uidNumber"] and shaped_info["identity"]["unix"]["gidNumber"]:
-        "Unix account present"
+        unix_status = "Unix account present"
     else:
-        "Unix account present"
+        unix_status = "Unix account not found"
+
+    shaped_info["unix_account_status"] = unix_status
     return {"user": shaped_info}
