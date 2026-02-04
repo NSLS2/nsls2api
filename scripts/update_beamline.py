47605332441304e586f9022c5bf4e97f85ecda9f
anubhutisinha04 <asinha1@bnl.gov>
2026-02-04 15:50:28 -0500

    Removed hardcoded values from the script

--- scripts/update_beamline.py
+++ scripts/update_beamline.py
@@ -9,7 +9,7 @@ from nsls2api.services import pass_service
 settings = get_settings()
 
 # CHANGE THIS TO THE BEAMLINE YOU WANT TO UPDATE
-BEAMLINE_NAME = "CDI"
+BEAMLINE_NAME = "TLA"
 
 
 async def main():
@@ -27,25 +27,30 @@ async def main():
 
     beamline = await Beamline.find_one(Beamline.pass_id == str(pass_ids[0]))
     if not beamline:
-        raise KeyError(f"No beamline found with pass_id {pass_ids[0]}")
+        raise KeyError(f"No beamline found with pass_id {pass_ids[0]}") 
+
+    print(beamline)
 
     # CHANGE THESE VALUES TO SUIT YOUR NEEDS.
     beamline.service_accounts = ServiceAccounts(
-        ioc="softioc-cdi",
-        epics_services="epics-services-cdi",
-        workflow="workflow-cdi",
-        bluesky="bluesky-cdi",
+        ioc="softioc-tla",
+        epics_services="epics-services-tla",
+        workflow="workflow-tla",
+        bluesky="bluesky-tla",
         operator="xf09id1",
         lsdc=None
     )
 
     print("Will update service_accounts for beamline:", beamline.name)
-    print(beamline)
 
     beamline.last_updated = datetime.datetime.now()
+    print(datetime.datetime.now())
 
     # Uncomment this line to actually save the changes to the database
-    await beamline.save()
+    # await beamline.save()
+
+    print("Updated beamline")
+    print(beamline)
 
 if __name__ == "__main__":
     asyncio.run(main())
